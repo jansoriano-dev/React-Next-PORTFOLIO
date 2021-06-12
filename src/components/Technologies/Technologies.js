10c58a06037b586722b3ea48da46d57f3a924d77
jan <jansoriano.programming@gmail.com>
2021-06-12 18:34:14 +0800

    2nd commit for portfolio

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiUnitySmall, DiReact, DiNodejsSmall } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+<Section id="tech">
+  <SectionDivider/>
+  <SectionTitle>Technologies</SectionTitle>
+  <SectionText>
+  Shopify Developer whose experience is on the front-end web development. Exploring full-stack web development mainly the MERN stack and has an interest in Django framework.
+  </SectionText>
+  <List>
+    <ListItem>
+      <DiReact size="3rem"/>
+      <ListContainer>
+        <ListContainer>
+          <ListTitle>Front-end</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Shopify,
+            ,React.js and
+            Django
+          </ListParagraph>
+        </ListContainer>
+      </ListContainer>
+    </ListItem>
+    <ListItem>
+      <DiNodejsSmall size="3rem"/>
+      <ListContainer>
+        <ListContainer>
+          <ListTitle>Backend-end</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Nodejs and databases
+          </ListParagraph>
+        </ListContainer>
+      </ListContainer>
+    </ListItem>
+    <ListItem>
+      <DiUnitySmall size="3rem"/>
+      <ListContainer>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+           tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListContainer>
+    </ListItem>
+  </List>
+</Section>
 );
 
 export default Technologies;
